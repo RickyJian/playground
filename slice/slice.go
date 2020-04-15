25cd3a29abe4a05c92191a3fda831f9e4cf6815e
rjbc <rjricky03bc@gmail.com>
2020-04-15 18:13:02 +0800

    add slice copy

--- slice/slice.go
+++ slice/slice.go
@@ -25,3 +25,21 @@ func NewSliceWithoutMake(no int) {
 		nos = append(nos, i)
 	}
 }
+
+func SliceCopy1(new, old []string) []string {
+	c := make([]string, len(new)+len(old))
+	for i, v := range new {
+		c[i] = v
+	}
+	for i, v := range old {
+		c[len(new)+i] = v
+	}
+	return c
+}
+
+func SliceCopy2(new, old []string) []string {
+	c := make([]string, len(new)+len(old))
+	copy(c, new)
+	copy(c[len(new):], old)
+	return c
+}
--- slice/slice_test.go
+++ slice/slice_test.go
@@ -2,6 +2,8 @@ package slice
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func BenchmarkNewSliceWithMake(b *testing.B) {
@@ -19,5 +21,51 @@ func BenchmarkNewSliceWithMakeAndSize(b *testing.B) {
 func BenchmarkNewSliceWithoutMake(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		NewSliceWithoutMake(i)
+	}
+}
+
+func TestSliceCopy1(t *testing.T) {
+	tests := []struct {
+		new      []string
+		old      []string
+		expected []string
+	}{
+		{
+			new:      []string{"1", "2", "3"},
+			old:      []string{"4", "5", "6"},
+			expected: []string{"1", "2", "3", "4", "5", "6"},
+		},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.expected, SliceCopy1(test.new, test.old))
+	}
+}
+
+func TestSliceCopy2(t *testing.T) {
+	tests := []struct {
+		new      []string
+		old      []string
+		expected []string
+	}{
+		{
+			new:      []string{"1", "2", "3"},
+			old:      []string{"4", "5", "6"},
+			expected: []string{"1", "2", "3", "4", "5", "6"},
+		},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.expected, SliceCopy2(test.new, test.old))
+	}
+}
+
+func BenchmarkSliceCopy1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SliceCopy1(make([]string, i), make([]string, i))
+	}
+}
+
+func BenchmarkSliceCopy2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SliceCopy2(make([]string, i), make([]string, i))
 	}
 }
