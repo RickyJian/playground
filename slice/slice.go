8375ac93a7609df34beadf6bdc8d29f4366d302a
rjbc <rjricky03bc@gmail.com>
2020-04-15 18:50:38 +0800

    add prepend

--- slice/slice.go
+++ slice/slice.go
@@ -43,3 +43,14 @@ func SliceCopy2(new, old []string) []string {
 	copy(c[len(new):], old)
 	return c
 }
+
+func Prepend1(no int, nos []int) []int {
+	return append([]int{no}, nos...)
+}
+
+func Prepend2(no int, nos []int) []int {
+	nos = append(nos, 0)
+	copy(nos[1:], nos)
+	nos[0] = no
+	return nos
+}
--- slice/slice_test.go
+++ slice/slice_test.go
@@ -69,3 +69,49 @@ func BenchmarkSliceCopy2(b *testing.B) {
 		SliceCopy2(make([]string, i), make([]string, i))
 	}
 }
+
+func TestPrepend1(t *testing.T) {
+	tests := []struct {
+		no       int
+		nos      []int
+		expected []int
+	}{
+		{
+			no:       1,
+			nos:      []int{2, 3, 4},
+			expected: []int{1, 2, 3, 4},
+		},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.expected, Prepend1(test.no, test.nos))
+	}
+}
+
+func TestPrepend2(t *testing.T) {
+	tests := []struct {
+		no       int
+		nos      []int
+		expected []int
+	}{
+		{
+			no:       1,
+			nos:      []int{2, 3, 4},
+			expected: []int{1, 2, 3, 4},
+		},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.expected, Prepend2(test.no, test.nos))
+	}
+}
+
+func BenchmarkPrepend1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Prepend1(i, make([]int, i+1))
+	}
+}
+
+func BenchmarkPrepend2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Prepend2(i, make([]int, i+1))
+	}
+}
