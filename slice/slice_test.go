932acde8b2d3c5b4648169c499dd889677218b4d
rjbc <rjricky03bc@gmail.com>
2020-04-15 17:02:23 +0800

    add new slice func

--- slice/slice_test.go
+++ slice/slice_test.go
@@ -1,1 +1,23 @@
 package slice
+
+import (
+	"testing"
+)
+
+func BenchmarkNewSliceWithMake(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		NewSliceWithMake(i)
+	}
+}
+
+func BenchmarkNewSliceWithMakeAndSize(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		NewSliceWithMakeAndSize(i)
+	}
+}
+
+func BenchmarkNewSliceWithoutMake(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		NewSliceWithoutMake(i)
+	}
+}
