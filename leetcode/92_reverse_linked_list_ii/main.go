0dfc87d14b8ecc294676f54dc4cb8c4246f1e0ff
RickyJian <rjricky03bc@gmail.com>
2022-05-30 00:17:55 +0800

    add comment

--- leetcode/92_reverse_linked_list_ii/main.go
+++ leetcode/92_reverse_linked_list_ii/main.go
@@ -85,20 +85,30 @@ func reverseBetween2(head *ListNode, left int, right int) *ListNode {
 		return head
 	}
 
+	// 將原先的 linked list 源頭增加一個節點，
+	// 預防從一第一個 node 開始時的判斷
 	dummy := &ListNode{Next: head}
-	//
 	prev := dummy
+	// 取到 left 前的節點
 	for i := 0; i < left-1; i++ {
 		prev = prev.Next
 	}
 
 	start := prev.Next
 	next := start.Next
+	// 每次 loop 將
+	//   1. 當下 node(start) 向後移，
+	//   2. 下個 node(next) 向前移
+	//   3. 前一個 node(prev) 指向下個 node(next)
+	//   4. 往當下的下個 node(start.Next) 移動
 	for i := 0; i < right-left; i++ {
-		// 跟下一個 node 做交換
+		// 當下 node 向後移
 		start.Next = next.Next
+		// 下個 node 向前移
 		next.Next = prev.Next
+		// 前一個 node 指向下個 node
 		prev.Next = next
+		// 往下個 node(start.Next) 移動
 		next = start.Next
 	}
 	return dummy.Next
