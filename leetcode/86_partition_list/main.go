993d6d10b28fd0597d3782f921d6474c40fd4be8
RickyJian <rjricky03bc@gmail.com>
2022-06-15 21:45:47 +0800

    add description

--- leetcode/86_partition_list/main.go
+++ leetcode/86_partition_list/main.go
@@ -9,19 +9,26 @@ type ListNode struct {
 }
 
 func partition(head *ListNode, x int) *ListNode {
-	first, beforeAfterX := &ListNode{}, &ListNode{}
-	beforeX, afterX := first, beforeAfterX
+	// first: 第一個節點，後續回傳使用
+	// firstAfterX: 第一個 val >= x 的節點，用來連結 val < x 最後的節點
+	first, firstAfterX := &ListNode{}, &ListNode{}
+	// beforeX: 保存所有 val < x 的 linked list
+	// afterX: 保存所有 val >= x 的 linked list
+	beforeX, afterX := first, firstAfterX
 	for head != nil {
-		if x > head.Val {
-			beforeX.Next = head
-			beforeX = beforeX.Next
-		} else {
+		if head.Val >= x {
 			afterX.Next = head
 			afterX = head
+		} else {
+			// head.Val < x
+			beforeX.Next = head
+			beforeX = beforeX.Next
 		}
 		head = head.Next
 	}
+	// 將最後一個節點指向 nil
 	afterX.Next = nil
-	beforeX.Next = beforeAfterX.Next
+	// 連結最後一個 beforeX 的節點與第一個 afterX
+	beforeX.Next = firstAfterX.Next
 	return first.Next
 }
