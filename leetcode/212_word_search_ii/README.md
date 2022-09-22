59260f4efc77122b71dbfa33ab3011dbb86324e3
RickyJian <rjricky03bc@gmail.com>
2022-09-22 22:56:11 +0800

    update description

--- leetcode/212_word_search_ii/README.md
+++ leetcode/212_word_search_ii/README.md
@@ -1,3 +1,8 @@
-# word search ii
+# 212 Word Search II
 
-// TODO: add description
+有一個 m x n 的英文字二維陣列以及一個單字陣列，請找出單字是否能從二維陣列中組合出。
+
+> 所有的單字皆必須由相鄰(垂直/水平)的字組合而成  
+> 走訪過的字只會使用一次
+
+[Word Search II](https://leetcode.com/problems/word-search-ii/)
--- leetcode/212_word_search_ii/main.go
+++ leetcode/212_word_search_ii/main.go
@@ -13,6 +13,9 @@ func main() {
 func findWords(board [][]byte, words []string) []string {
 	trie := new(node)
 	for _, word := range words {
+		// 應用 trie 的特性將單字分割成各個英文字母，
+		// 待後續走訪 board 時判斷是否能組成單字，
+		// 並且減少走訪重複前綴的 word
 		trie.insert(word)
 	}
 
@@ -29,34 +32,42 @@ const wordBeenUsed = '.'
 
 func dfs(board [][]byte, i, j int, trie *node, result *[]string) {
 	letter := board[i][j]
+	fmt.Printf("i: %d, j: %d, letter: %s\n", i, j, string(letter))
 	if letter == wordBeenUsed || trie.children[letter-'a'] == nil {
+		// 若遇到以使用過的 cell 或是在 trie 中查無節點，
+		// 代表所走訪的 cell 無法組合成單字
 		return
 	}
 	n := trie.children[letter-'a']
 	if n.word != "" {
+		// 若查找到單字將該單字寫入 result 中，
 		*result = append(*result, n.word)
-		// remove used word
+		// 將查找到的單字清除，防止後續找到相同組合
 		n.word = ""
+		// 有可能會有同時存在前掇綴字相同的單字在此不須回傳
 	}
 
+	// 將走訪過的 cell 更成特殊符號，若後續遇到此符號則代表已到盡頭即可跳出 dfs
 	board[i][j] = wordBeenUsed
 	if i > 0 {
-		// 向後找
+		// 向上找
 		dfs(board, i-1, j, n, result)
 	}
 	if j > 0 {
-		// 向上找
+		// 向前找
 		dfs(board, i, j-1, n, result)
 	}
+	// -1: 最後一個節點不須走訪
 	if i < len(board)-1 {
-		// 向前找
+		// 向下找
 		dfs(board, i+1, j, n, result)
 	}
+	// -1: 最後一個節點不須走訪
 	if j < len(board[0])-1 {
-		// 向下找
+		// 向後找
 		dfs(board, i, j+1, n, result)
 	}
-	// 結束須將值回復
+	// 將走訪過的 cell 復原
 	board[i][j] = letter
 }
 
