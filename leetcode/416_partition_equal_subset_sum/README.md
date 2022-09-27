77e58f3de5cdf864985df00d0211cfa34be289a7
RickyJian <rjricky03bc@gmail.com>
2022-09-28 01:09:08 +0800

    add dp solution

--- leetcode/416_partition_equal_subset_sum/README.md
+++ leetcode/416_partition_equal_subset_sum/README.md
@@ -3,3 +3,8 @@
 給定一個非空的正整數正列，是否能將此陣列劃分成兩組且各組元素加總皆為相同。
 
 [Partition Equal Subset Sum](https://leetcode.com/problems/partition-equal-subset-sum/)
+
+-------------------
+## Algorithm
+
+0/1 Knapsack Problem
--- leetcode/416_partition_equal_subset_sum/main_test.go
+++ leetcode/416_partition_equal_subset_sum/main_test.go
@@ -28,3 +28,26 @@ func TesCcanPartitionDFS(t *testing.T) {
 		assert.Equal(t, test.expected, canPartitionDFS(test.nums))
 	}
 }
+
+func TesCcanPartitionDP(t *testing.T) {
+	var tests = []struct {
+		nums     []int
+		expected bool
+	}{
+		{
+			nums:     []int{1, 5, 11, 5},
+			expected: true,
+		},
+		{
+			nums:     []int{1, 2, 3, 5},
+			expected: false,
+		},
+		{
+			nums:     []int{2, 2, 1, 1},
+			expected: true,
+		},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.expected, canPartitionDP(test.nums))
+	}
+}
