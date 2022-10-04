edb623f2676c5e345619c855756146055770d329
RickyJian <rjricky03bc@gmail.com>
2022-10-05 00:00:02 +0800

    add description

--- leetcode/1155_number_of_dice_rolls_with_target_sum/main.go
+++ leetcode/1155_number_of_dice_rolls_with_target_sum/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
@@ -11,15 +10,27 @@ func main() {
 }
 
 func numRollsToTargetDP(n int, k int, target int) int {
-	m := int(math.Pow10(9) + 7)
+	// 若 n 個骰子的最大值都無超過 target 代表骰子終究無法組合成 target
+	if n*k < target {
+		return 0
+	}
+
+	m := 1000000007
 	dp := make([][]int, n+1)
 	for i := 0; i <= n; i++ {
 		dp[i] = make([]int, target+1)
 	}
+	// dp[i][j]：表示有 i 顆骰子可以組成 j 的方法有幾種
+	// 雖然題目不會有 dp[0][0] 的情形發生，但為了讓後續 dp 可以向下執行需要個初始值，
+	// 而我們可以看作當有 0 顆骰子可以組成 0 的方法有 1 種
 	dp[0][0] = 1
 	for i := 1; i <= n; i++ {
 		for j := 1; j <= k; j++ {
+			// 我們只須檢查 z ~ target 有幾種組合方式，
+			// 因為 1 ~ (z - 1) 會是我們 j -1 執行的結果
 			for z := j; z <= target; z++ {
+				// TODO: add description
+				// dp[i-1][z-j]：檢查上一顆骰子骰出的結果有幾種方法可以組成 z-j
 				dp[i][z] = (dp[i][z] + dp[i-1][z-j]) % m
 			}
 		}
