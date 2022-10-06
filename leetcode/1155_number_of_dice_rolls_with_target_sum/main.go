b71be133b1539222299e11970bd225b262a20c77
RickyJian <rjricky03bc@gmail.com>
2022-10-06 23:48:53 +0800

    add dfs with memoization

--- leetcode/1155_number_of_dice_rolls_with_target_sum/main.go
+++ leetcode/1155_number_of_dice_rolls_with_target_sum/main.go
@@ -5,13 +5,21 @@ import (
 )
 
 func main() {
-	fmt.Println(numRollsToTargetDP(1, 6, 3))
+	// fmt.Println(numRollsToTargetDP(1, 6, 3))
 	fmt.Println(numRollsToTargetDP(2, 6, 7))
-	fmt.Println(numRollsToTargetDP(3, 6, 7))
+	// fmt.Println(numRollsToTargetDP(3, 6, 7))
+	// fmt.Println(numRollsToTargetDP(30, 30, 500))
+	// fmt.Println(numRollsToTargetDP(10, 10, 30))
 	fmt.Println("====")
-	fmt.Println(numRollsToTargetDFS(1, 6, 3))
-	fmt.Println(numRollsToTargetDFS(2, 6, 7))
-	fmt.Println(numRollsToTargetDFS(3, 6, 7))
+	// fmt.Println(numRollsToTargetDFS(1, 6, 3))
+	// fmt.Println(numRollsToTargetDFS(2, 6, 7))
+	// fmt.Println(numRollsToTargetDFS(3, 6, 7))
+	// fmt.Println(numRollsToTargetDP(30, 30, 500))
+	fmt.Println("====")
+	// fmt.Println(numRollsToTargetDFSMemo(1, 6, 3))
+	// fmt.Println(numRollsToTargetDFSMemo(2, 6, 7))
+	// fmt.Println(numRollsToTargetDFSMemo(3, 6, 7))
+	// fmt.Println(numRollsToTargetDFSMemo(30, 30, 500))
 }
 
 func numRollsToTargetDP(n int, k int, target int) int {
@@ -34,7 +42,7 @@ func numRollsToTargetDP(n int, k int, target int) int {
 			// 我們只須檢查 z ~ target 有幾種組合方式，
 			// 因為 1 ~ (z - 1) 會是我們 j -1 執行的結果
 			for z := j; z <= target; z++ {
-				// TODO: add description
+				// 不計算 j < z 是因為骰出的值已經大於 target，結果必為 0
 				// dp[i-1][z-j]：檢查上一顆骰子骰出的結果有幾種方法可以組成 z-j
 				dp[i][z] = (dp[i][z] + dp[i-1][z-j]) % m
 			}
@@ -70,4 +78,30 @@ func dfs(n, k, target int) int {
 	return count
 }
 
-// TODO: dfs with memo
+func numRollsToTargetDFSMemo(n int, k int, target int) int {
+	// 若 n 個骰子的最大值都無超過 target 代表骰子終究無法組合成 target
+	if n*k < target {
+		return 0
+	}
+	return dfsMemo(n, k, target, make(map[string]int))
+}
+
+func dfsMemo(n, k, target int, memo map[string]int) int {
+	key := fmt.Sprintf("%d_%d", n, target)
+	if target == 0 && n == 0 {
+		// 為最後一個骰子且剛好可以組合成 target
+		return 1
+	} else if target < 0 || n == 0 {
+		// 為最後一個骰子 或 target 無法組合
+		return 0
+	} else if val, ok := memo[key]; ok {
+		// TODO: description
+		return val
+	}
+
+	for i := 1; i <= k; i++ {
+		// n-1：骰子骰過不可再用
+		memo[key] = (memo[key] + dfsMemo(n-1, k, target-i, memo)) % 1000000007
+	}
+	return memo[key]
+}
