e78b22e743f92e5dd72f35b9b5491c1e71095475
RickyJian <rjricky03bc@gmail.com>
2022-11-06 17:31:53 +0800

    add brute force solution

--- leetcode/795_number_of_subarrays_with_bounded_maximum/main.go
+++ leetcode/795_number_of_subarrays_with_bounded_maximum/main.go
@@ -3,3 +3,34 @@ package main
 func main() {
 	// TODO: implement here
 }
+
+func numSubarrayBoundedMaxBruteForce(nums []int, left int, right int) int {
+	var result int
+	for i := 0; i < len(nums); i++ {
+		l := nums[i]
+		// 當最左邊的元素 > 右邊邊界
+		if l > right {
+			continue
+		}
+
+		// 子陣列最大元素
+		max := l
+		for j := i; j < len(nums); j++ {
+			r := nums[j]
+			// 若當下元素 > 右邊邊界則不須走訪後續元素
+			if r > right {
+				break
+			}
+
+			// 若當下元素 > max 將該元素替換成 max
+			if max < r {
+				max = r
+			}
+			// 當子陣列最大元素 >= 左方邊界滿足題目要求結果須 + 1
+			if max >= left {
+				result++
+			}
+		}
+	}
+	return result
+}
--- leetcode/795_number_of_subarrays_with_bounded_maximum/main_test.go
+++ leetcode/795_number_of_subarrays_with_bounded_maximum/main_test.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var tests = []*struct {
+	nums     []int
+	left     int
+	right    int
+	expected int
+}{
+	{
+		nums:     []int{2, 1, 4, 3},
+		left:     2,
+		right:    3,
+		expected: 3,
+	},
+	{
+		nums:     []int{2, 9, 2, 5, 6},
+		left:     2,
+		right:    8,
+		expected: 7,
+	},
+	{
+		nums:     []int{73, 55, 36, 5, 55, 14, 9, 7, 72, 52},
+		left:     32,
+		right:    69,
+		expected: 22,
+	},
+}
+
+func TestNumSubarrayBoundedMaxBruteForce(t *testing.T) {
+	for _, test := range tests {
+		assert.Equal(t, test.expected, numSubarrayBoundedMaxBruteForce(test.nums, test.left, test.right))
+	}
+}
