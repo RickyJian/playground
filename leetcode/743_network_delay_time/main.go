70534a7342e8e40484a49276fa2251e4a06e3e6a
RickyJian <rjricky03bc@gmail.com>
2022-11-20 16:25:16 +0800

    add regular dfs solution

--- leetcode/743_network_delay_time/main.go
+++ leetcode/743_network_delay_time/main.go
@@ -1,5 +1,38 @@
 package main
 
 func main() {
-	// TODO: implement here
+}
+
+func networkDelayTime(times [][]int, n int, k int) int {
+	nodes := make(map[int][][]int)
+	for _, time := range times {
+		nodes[time[0]] = append(nodes[time[0]], []int{time[1], time[2]})
+	}
+
+	minPath := make(map[int]int)
+	reached := make(map[int]struct{})
+	dfs(nodes, reached, minPath, k, 0)
+	if len(reached) != n {
+		return -1
+	}
+
+	var max int
+	for _, path := range minPath {
+		if max < path {
+			max = path
+		}
+	}
+	return max
+}
+
+func dfs(nodes map[int][][]int, reached map[int]struct{}, minPath map[int]int, from, weight int) {
+	reached[from] = struct{}{}
+	if minWeight, ok := minPath[from]; ok && minWeight <= weight {
+		return
+	}
+
+	minPath[from] = weight
+	for _, children := range nodes[from] {
+		dfs(nodes, reached, minPath, children[0], children[1]+weight)
+	}
 }
--- leetcode/743_network_delay_time/main_test.go
+++ leetcode/743_network_delay_time/main_test.go
@@ -1,1 +1,56 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var tests = []*struct {
+	times    [][]int
+	n        int
+	k        int
+	expected int
+}{
+	{
+		times: [][]int{
+			{2, 1, 1},
+			{2, 3, 1},
+			{3, 4, 1},
+		},
+		n:        4,
+		k:        2,
+		expected: 2,
+	},
+	{
+		times: [][]int{
+			{1, 2, 1},
+		},
+		n:        2,
+		k:        1,
+		expected: 1,
+	},
+	{
+		times: [][]int{
+			{1, 2, 1},
+		},
+		n:        2,
+		k:        2,
+		expected: -1,
+	},
+	{
+		times: [][]int{
+			{1, 2, 1},
+			{2, 1, 3},
+		},
+		n:        2,
+		k:        2,
+		expected: 3,
+	},
+}
+
+func TestNetworkDelayTimeDFS(t *testing.T) {
+	for _, test := range tests {
+		assert.Equal(t, test.expected, networkDelayTime(test.times, test.n, test.k))
+	}
+}
