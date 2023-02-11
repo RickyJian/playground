d6fbb1c9a60b9fc951a856d586fd9fbe5501570b
RickyJian <rjricky03bc@gmail.com>
2023-02-11 19:44:01 +0800

    add dfs solution

--- leetcode/886_possible_bipartition/main.go
+++ leetcode/886_possible_bipartition/main.go
@@ -5,16 +5,41 @@ func main() {
 }
 
 // DFS solution
+// refer to: https://en.wikipedia.org/wiki/Bipartite_graph
 func possibleBipartitionDFS(n int, dislikes [][]int) bool {
-	return false
+	graph := make([][]int, n)
+	for _, nodes := range dislikes {
+		n1, n2 := nodes[0]-1, nodes[1]-1
+		graph[n1] = append(graph[n1], n2)
+		graph[n2] = append(graph[n2], n1)
+	}
+
+	colors := make([]int, n)
+	for i := 0; i < n; i++ {
+		if colors[i] == 0 {
+			if !dfs(graph, i, 1, colors) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func dfs(graph [][]int, node int, color int, colors []int) bool {
+	colors[node] = color
+	for _, nextNode := range graph[node] {
+		if nextColor := colors[nextNode]; nextColor == 0 {
+			if !dfs(graph, nextNode, -color, colors) {
+				return false
+			}
+		} else if nextColor == color {
+			return false
+		}
+	}
+	return true
 }
 
 // Union Find solution
 func possibleBipartitionUF(n int, dislikes [][]int) bool {
-	return false
-}
-
-// Bitpartition solution
-func possibleBipartition(n int, dislikes [][]int) bool {
 	return false
 }
--- leetcode/886_possible_bipartition/main_test.go
+++ leetcode/886_possible_bipartition/main_test.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
 var tests = []*struct {
 	n        int
 	dislikes [][]int
@@ -13,6 +19,17 @@ var tests = []*struct {
 	{
 		n:        3,
 		dislikes: [][]int{{1, 2}, {1, 3}, {2, 3}},
-		expected: true,
+		expected: false,
 	},
+	{
+		n:        5,
+		dislikes: [][]int{{1, 2}, {3, 4}, {4, 5}, {3, 5}},
+		expected: false,
+	},
+}
+
+func TestPossibleBipartitionDFS(t *testing.T) {
+	for _, test := range tests {
+		assert.Equal(t, test.expected, possibleBipartitionDFS(test.n, test.dislikes))
+	}
 }
