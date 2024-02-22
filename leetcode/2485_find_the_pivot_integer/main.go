5478d2dc4a93f8c3af9a73ee932266b32fa98203
RickyJian <rjricky03bc@gmail.com>
2024-02-22 23:54:38 +0800

    add solution

--- leetcode/2485_find_the_pivot_integer/main.go
+++ leetcode/2485_find_the_pivot_integer/main.go
@@ -3,3 +3,27 @@ package main
 func main() {
 	// TODO: implement here
 }
+
+// brute force
+func pivotIntegerV1(n int) int {
+	// init
+	numbers := make([]int, n)
+	for i := 1; i <= n; i++ {
+		numbers[i-1] = i
+	}
+	// calculate
+	for i := 0; i < n; i++ {
+		left, right := numbers[:i+1], numbers[i:]
+		var total int
+		for _, val := range left {
+			total += val
+		}
+		for _, val := range right {
+			total -= val
+		}
+		if total == 0 {
+			return i + 1
+		}
+	}
+	return -1
+}
--- leetcode/2485_find_the_pivot_integer/main_test.go
+++ leetcode/2485_find_the_pivot_integer/main_test.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var tests = []*struct {
+	n        int
+	expected int
+}{
+	{
+		n:        8,
+		expected: 6,
+	},
+	{
+		n:        1,
+		expected: 1,
+	},
+	{
+		n:        4,
+		expected: -1,
+	},
+}
+
+func TestPivotInteger(t *testing.T) {
+	for _, test := range tests {
+		assert.Equal(t, test.expected, pivotIntegerV1(test.n))
+	}
+}
