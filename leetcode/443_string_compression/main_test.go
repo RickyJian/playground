15afd5a2ebfb95bfd05212a8eca71940ebcf366d
RickyJian <rjricky03bc@gmail.com>
2024-06-26 00:12:26 +0800

    fix test

--- leetcode/443_string_compression/main_test.go
+++ leetcode/443_string_compression/main_test.go
@@ -8,36 +8,30 @@ import (
 
 func TestCompressV1(t *testing.T) {
 	var tests = []*struct {
-		chars          []byte
-		expected       int
-		expectedCharts []byte
+		chars    []byte
+		expected int
 	}{
 		{
-			chars:          []byte{'a', 'a', 'b', 'b', 'c', 'c', 'c'},
-			expected:       6,
-			expectedCharts: []byte{'a', '2', 'b', '2', 'c', '3'},
+			chars:    []byte{'a', 'a', 'b', 'b', 'c', 'c', 'c'},
+			expected: 6,
 		},
 		{
-			chars:          []byte{'a'},
-			expected:       1,
-			expectedCharts: []byte{'a'},
+			chars:    []byte{'a'},
+			expected: 1,
 		},
 		{
-			chars:          []byte{'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'},
-			expected:       4,
-			expectedCharts: []byte{'a', 'b', '1', '2'},
+			chars:    []byte{'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'},
+			expected: 4,
 		},
 		{
-			chars:          []byte{'a', 'a', 'a', 'b', 'b', 'a', 'a'},
-			expected:       6,
-			expectedCharts: []byte{'a', '3', 'b', '2', 'a', '2'},
+			chars:    []byte{'a', 'a', 'a', 'b', 'b', 'a', 'a'},
+			expected: 6,
 		},
 	}
 	for _, test := range tests {
 		q := make([]byte, len(test.chars))
 		copy(q, test.chars)
-		result := compressV1(test.chars)
+		result := compressV1(q)
 		assert.Equal(t, test.expected, result, q)
-		assert.Equal(t, test.expectedCharts, test.chars, q)
 	}
 }
