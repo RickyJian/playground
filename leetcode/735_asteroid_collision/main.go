c6489ef8696fdb7b820f39184ade44b5a0ec0a48
RickyJian <rjricky03bc@gmail.com>
2024-08-21 00:00:15 +0800

    add second solution

--- leetcode/735_asteroid_collision/main.go
+++ leetcode/735_asteroid_collision/main.go
@@ -40,3 +40,20 @@ func asteroidCollisionV1(asteroids []int) []int {
 func isNotCollision(l, r int) bool {
 	return (l > 0 && r > 0) || (l < 0 && r < 0) || (l < 0 && r > 0)
 }
+
+// refer to: https://leetcode.com/problems/asteroid-collision/solutions/4025988/go-solution-easy-explanation-o-n-time-space-complexity/?envType=study-plan-v2&envId=leetcode-75
+func asteroidCollisionV2(asteroids []int) []int {
+	var stack []int
+	for i := 0; i < len(asteroids); i++ {
+		asteroid := asteroids[i]
+		if len(stack) == 0 || stack[len(stack)-1] < 0 || asteroid > 0 {
+			stack = append(stack, asteroid)
+		} else if prev := stack[len(stack)-1]; prev == -asteroid {
+			stack = stack[:len(stack)-1]
+		} else if prev < -asteroid {
+			stack = stack[:len(stack)-1]
+			i--
+		}
+	}
+	return stack
+}
--- leetcode/735_asteroid_collision/main_test.go
+++ leetcode/735_asteroid_collision/main_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAsteroidCollision(t *testing.T) {
+func TestAsteroidCollisionV1(t *testing.T) {
 	var tests = []*struct {
 		asteroids []int
 		expected  []int
@@ -38,5 +38,40 @@ func TestAsteroidCollision(t *testing.T) {
 	}
 	for _, test := range tests {
 		assert.Equal(t, test.expected, asteroidCollisionV1(test.asteroids), test.asteroids)
+	}
+}
+
+func TestAsteroidCollisionV2(t *testing.T) {
+	var tests = []*struct {
+		asteroids []int
+		expected  []int
+	}{
+		{
+			asteroids: []int{5, 10, -5},
+			expected:  []int{5, 10},
+		},
+		{
+			asteroids: []int{8, -8},
+			expected:  []int{},
+		},
+		{
+			asteroids: []int{10, 2, -5},
+			expected:  []int{10},
+		},
+		{
+			asteroids: []int{-2, -1, 1, 2},
+			expected:  []int{-2, -1, 1, 2},
+		},
+		{
+			asteroids: []int{-2, -2, 1, -2},
+			expected:  []int{-2, -2, -2},
+		},
+		{
+			asteroids: []int{1, -2, -2, -2},
+			expected:  []int{-2, -2, -2},
+		},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.expected, asteroidCollisionV2(test.asteroids), test.asteroids)
 	}
 }
