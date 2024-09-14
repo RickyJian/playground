05682c44a61b994f12d9d34587555e93d051792f
RickyJian <rjricky03bc@gmail.com>
2024-09-14 20:16:57 +0800

    add middle node and reverse solution

--- leetcode/2130_maximum_twin_sum_of_a_linked_list/main.go
+++ leetcode/2130_maximum_twin_sum_of_a_linked_list/main.go
@@ -23,6 +23,51 @@ func pairSumV1(head *ListNode) int {
 	return max
 }
 
+func pairSumV2(head *ListNode) int {
+	// find middle node
+	slow := head
+	fast := head.Next
+	for {
+		if fast == nil || fast.Next == nil {
+			break
+		}
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	// keep after middle node
+	right := slow.Next
+	// remove link to next node, due to we need to reverse left nodes
+	slow.Next = nil
+	// reverse slow
+	left := reverse(nil, head)
+
+	// sum twin
+	var max int
+	for {
+		if right == nil {
+			break
+		}
+		sum := left.Val + right.Val
+		if sum > max {
+			max = sum
+		}
+		left = left.Next
+		right = right.Next
+	}
+	return max
+}
+
+func reverse(prev, head *ListNode) *ListNode {
+	if head == nil {
+		return prev
+	}
+
+	next := head.Next
+	head.Next = prev
+	return reverse(head, next)
+}
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
--- leetcode/2130_maximum_twin_sum_of_a_linked_list/main_test.go
+++ leetcode/2130_maximum_twin_sum_of_a_linked_list/main_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestOddEvenListV1(t *testing.T) {
+func TestPairSumV1(t *testing.T) {
 	var tests = []*struct {
 		head     *ListNode
 		expected int
@@ -44,5 +44,46 @@ func TestOddEvenListV1(t *testing.T) {
 	}
 	for _, test := range tests {
 		assert.Equal(t, test.expected, pairSumV1(test.head), test.head.Val)
+	}
+}
+
+func TestPairSumV2(t *testing.T) {
+	var tests = []*struct {
+		head     *ListNode
+		expected int
+	}{
+		{
+			head: &ListNode{
+				Val: 5,
+				Next: &ListNode{
+					Val: 4,
+					Next: &ListNode{
+						Val: 2,
+						Next: &ListNode{
+							Val: 1,
+						},
+					},
+				},
+			},
+			expected: 6,
+		},
+		{
+			head: &ListNode{
+				Val: 4,
+				Next: &ListNode{
+					Val: 2,
+					Next: &ListNode{
+						Val: 2,
+						Next: &ListNode{
+							Val: 3,
+						},
+					},
+				},
+			},
+			expected: 7,
+		},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.expected, pairSumV2(test.head), test.head.Val)
 	}
 }
